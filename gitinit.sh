1be9dfed085148c7535a73167ab323f8e2043a8d
EC2 Default User <ec2-user@ip-172-31-16-182.us-west-2.compute.internal>
2015-04-20 05:22:37 +0000

    first commit

---
+++ gitinit.sh
@@ -0,0 +1,6 @@
+echo "# ramsey_search" >> README.md
+git init
+git add *
+git commit -m "first commit"
+git remote add origin https://github.com/dbjnbnrj/ramsey_search.git
+git push -u origin master
