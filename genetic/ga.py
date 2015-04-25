c26072c161f12d9fc70ebcf8dae6c244949dfc28
Sachin Rathod <rathodsachin20@gmail.com>
2015-04-24 21:52:21 -0700

    modified genetic algo for increasing number of nodes.

--- genetic/ga.py
+++ genetic/ga.py
@@ -5,18 +5,23 @@ def individual(dim):
     return get_nparr(dim)
     #return [randint(low, high) for i in xrange(length)]
 
-def population(count, dim):
+def population(count, dim, pop_in=None):
+    if pop_in is not None:
+        return pop_in
     return [individual(dim) for i in xrange(count)]
 
 def fitness(individual, target):
     #score = sum(individual)
     score = clique_count(individual)
     if score==0:
-        print "Found counter-example of size:", len(individual)
-        print individual
+        #print "Found counter-example of size:", len(individual)
+        if(len(individual))>50:
+            print individual
     return abs(score - target)
 
-def grade(population, target):
+def grade(population, target, graded_list=None):
+    if graded_list is not None:
+        return min(graded_lis)[0]
     return min([fitness(i, target) for i in population])
     #total = sum([fitness(i, target) for i in population])
     #return total/(len(population)*1.0)
@@ -42,6 +47,8 @@ def evolve(pop, target=0, retain=0.2, random_select=0.05, mutate=0.1):
     #print graded
     #graded = [x[1] for x in sorted(graded)]
     g = sorted(graded, key=lambda x:x[0])
+    counts = [x[0] for x in g]
+    num_zeros = counts.count(0)
     graded = [x[1] for x in g]
     retain_length = int(retain*len(graded))
     parents = graded[:retain_length]
@@ -87,24 +94,45 @@ def evolve(pop, target=0, retain=0.2, random_select=0.05, mutate=0.1):
             children.append(child)
 
     parents.extend(children)
-    return parents
+    return (parents, num_zeros)
 
 
-
-def ramsey_ga(dim, pop_count, generations):
-    a = get_nparr(dim)
+def ramsey_ga_onegen(pop_in, dim, pop_count, generations):
+    #a = get_nparr(dim)
     target = 0
-    p = population(pop_count, dim)
-    fitness_history = [grade(p,target),]
+    p = population(pop_count, dim, pop_in)
+    #fitness_history = [grade(p,target),]
 
     for i in xrange(generations):
-        p = evolve(p, target)
-        gr = grade(p, target)
-        print "poulation score:", gr
-        fitness_history.append(gr)
+        (p, zero_count) = evolve(p, target)
+        #gr = grade(p, target)
+        print "dim:", dim, "  poulation #counter-examples:", zero_count
+        if zero_count>=10:
+            break
+        #fitness_history.append(gr)
 
-    for line in fitness_history:
-        print line
+    #for line in fitness_history:
+    #    print line
+    #print fitness_history
+    return p
+
+def ramsey_ga(start_dim, end_dim, pop_count, generations):
+    pop_start = population(pop_count, start_dim)
+    pop_cur = pop_start
+    for dim in range(start_dim, end_dim+1):
+        print "Evolving Generation of dimension:", dim
+        pop_old = ramsey_ga_onegen(pop_cur, dim, pop_count, generations)
+        #pop_new = np.zeros((dim+1,dim+1), dtype=int)
+        pop_new = []
+        for indiv in pop_old:
+            indiv_new = np.zeros((dim+1, dim+1), dtype=int)
+            indiv_new[0:dim, 0:dim] = indiv
+            for i in xrange(dim+1):
+                indiv_new[i][dim] = randint(0,1)
+                indiv_new[dim][i] = indiv_new[i][dim]
+            pop_new.append(indiv_new)
+        pop_cur = pop_new
+        dim = dim +1
 
 if __name__ == "__main__":
     #target = 371
@@ -121,7 +149,8 @@ if __name__ == "__main__":
 
     #for line in fitness_history:
     #    print line
-    graph_size = 55
+    start = 20
+    stop = 70
     pop = 100
     gen = 100
-    ramsey_ga(graph_size, pop, gen)
+    ramsey_ga(start, stop, pop, gen)
