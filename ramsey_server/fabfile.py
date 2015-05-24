e0d087c52c0c43bc987059f403cf2226e7c6e653
Debjani Banerjee <dbjnbnrj@gmail.com>
2015-05-24 14:15:19 -0700

    Adding fabfile

--- ramsey_server/fabfile.py
+++ ramsey_server/fabfile.py
@@ -7,19 +7,62 @@ import json
 from ilogue.fexpect import expect, expecting, run 
 from fabric.contrib.files import exists
 
-env.hosts = ["52.10.198.153"]#, "52.24.235.237", "52.25.104.145"]
-env.user = "ec2-user"
-env.key_filename = "ramsey.pem"
-env.port = 22
+
+host_file = open('server_azure.json')
+host_result = json.load(host_file)
+
+if "hosts" in host_result:
+	env.hosts = host_result["hosts"]
+if "password" in host_result:
+	env.passwords = host_result["passwords"]
+if "key_filename" in host_result:
+	env.key_filename = host_result["key_filename"]
+if "ipaddress" in host_result:
+	ipaddress = host_result["ipaddress"]
+
+code_dir = host_result["code_dir"]
+
+prompts = []
+prompts += expect("Username for 'https://github.com'",'dbjnbnrj')
+prompts += expect("Password for 'https://dbjnbnrj@github.com'",'')
 
 
-def startserver():
-  sudo("cd ramsey_search/ramsey_server")
-  sudo('screen -d -m python server.py; sleep 1')
-  sudo('cat out.txt')
+def run_bg(cmd, before=None, sockname="dtach", use_sudo=False):
+    if not exists("/usr/bin/screen"):
+        sudo("apt-get install screen")
+    if before:
+        cmd = "{}; dtach -n `mktemp -u /tmp/{}.XXXX` {}".format(
+            before, sockname, cmd)
+    else:
+        cmd = "dtach -n `mktemp -u /tmp/{}.XXXX` {}".format(sockname, cmd)
+    if use_sudo:
+        return sudo(cmd)
+    else:
+        return run(cmd)
 
+def start_server():
+	#run("lsof -P | grep ':8888' | awk '{print $2}' | xargs kill -9")
+	with cd(code_dir):
+		sudo("git pull")
+	  	sudo('screen -d python server.py; sleep 1')
 
-def startclients():
-  sudo("cd ramsey_search/ramsey_client")
-  sudo('screen -d -m python server.py; sleep 1')
-  sudo('cat out.txt')
+@parallel
+def start_client():
+	with cd(code_dir):
+			run("make clean")
+			run("make all")
+			sudo('screen -d python client_2.py '+ipaddress+'; sleep 1')
+
+def setup_client():
+		sudo("apt-get install python-pip python-dev build-essential")
+		sudo("apt-get install screen")
+		sudo("pip install pexpect")
+		if exists(code_dir):
+			sudo("rm -rf "+code_dir)
+		with expecting(prompts):
+			sudo("git clone https://github.com/dbjnbnrj/ramsey_search.git")
+		with cd(code_dir):
+			sudo("make clean")
+
+def check():
+	run("ls")
